cf84018b52eef1ca0beeafb88c918355e896b9d8
Jeritano <8475958+Jeritano@users.noreply.github.com>
2026-06-12 13:48:36 -0400

    froglips 0.13.4

--- Casks/froglips.rb
+++ Casks/froglips.rb
@@ -1,6 +1,6 @@
 cask "froglips" do
-  version "0.13.3"
-  sha256 "27724138dab744bb85d95d9b21b3a227b7bc603af77ca6fd4a016208a2a262ce"
+  version "0.13.4"
+  sha256 "d4e0639dd5094a2cc09ec9f5ef7cf7a65911d3c9785107174001c0764a0f9e0e"
 
   url "https://github.com/Jeritano/FrogLips/releases/download/v#{version}/Froglips_#{version}_aarch64.dmg"
   name "Froglips"
